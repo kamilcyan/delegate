206cedaa2460090a5080cb1aaacebed576b73f69
kamilcyan <kamil.lucjanek@wp.pl>
2020-01-27 18:11:17 +0100

    rty

---
+++ ConsoleApp1/ConsoleApp1/MySecondObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class MySecondObject
+    {
+        public void print(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        public MySecondObject(Simulace simulace)
+        {
+
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Simulace.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Simulace
+    {
+        public int CurrentTime;
+
+        public Simulace()
+        {
+            CurrentTime = 0;
+        }
+
+        public void simulate()
+        {
+            while(CurrentTime < 10)
+            {
+                Console.WriteLine(CurrentTime);
+                CurrentTime++;
+            }
+        }
+    }
+}
